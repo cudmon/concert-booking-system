c91ba3815a97beeac25face85ecfd664e4232e0b
Tonsak <tonsak.gs@gmail.com>
2026-05-21 18:31:22 +0700

    feat: add `jwt`, `config`, `typeorm` module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,6 +1,36 @@
+import { JwtModule } from "@nestjs/jwt";
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
-  imports: []
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres",
+        autoLoadEntities: true,
+        host: configService.get<string>("DB_HOST"),
+        port: configService.get<number>("DB_PORT"),
+        database: configService.get<string>("DB_NAME"),
+        username: configService.get<string>("DB_USERNAME"),
+        password: configService.get<string>("DB_PASSWORD")
+      })
+    }),
+
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>("JWT_SECRET"),
+        signOptions: {
+          expiresIn: "1h"
+        }
+      })
+    })
+  ]
 })
 export class AppModule {}
