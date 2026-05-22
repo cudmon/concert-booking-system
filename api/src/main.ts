780b2a0a2bbbcc4c177f616bd1a5935823e2e6cf
Tonsak <tonsak.gs@gmail.com>
2026-05-22 12:48:32 +0700

    feat: setup CORS, global prefix, shutdown hooks, and validation/interceptor configuration

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,24 @@
 import { AppModule } from "@/app.module";
-import { NestFactory } from "@nestjs/core";
+import { NestFactory, Reflector } from "@nestjs/core";
+import { ClassSerializerInterceptor, ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  app.setGlobalPrefix("/api");
+
+  app.enableShutdownHooks();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true
+    })
+  );
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   await app.listen(process.env.PORT ?? 5000);
 }
