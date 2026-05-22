524b6bee040b3043ef51d90fe6bf390912f0c5c3
Tonsak <tonsak.gs@gmail.com>
2026-05-22 12:51:11 +0700

    refactor: clean up app module by removing unused JwtModule and ConfigService imports

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,26 +1,24 @@
-import { JwtModule } from "@nestjs/jwt";
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { AuthModule } from "@/features/auth/auth.module";
 import { dataSourceOptions } from "@/database/datasource";
-import { ConfigModule, ConfigService } from "@nestjs/config";
+import { UsersModule } from "@/features/users/users.module";
+import { OrdersModule } from "@/features/orders/orders.module";
+import { ConcertsModule } from "@/features/concerts/concerts.module";
 
 @Module({
   imports: [
+    AuthModule,
+    UsersModule,
+    OrdersModule,
+    ConcertsModule,
+
     ConfigModule.forRoot({
       isGlobal: true
     }),
 
-    TypeOrmModule.forRoot(dataSourceOptions),
-
-    JwtModule.registerAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        secret: configService.getOrThrow<string>("JWT_SECRET"),
-        signOptions: {
-          expiresIn: "1h"
-        }
-      })
-    })
+    TypeOrmModule.forRoot(dataSourceOptions)
   ]
 })
 export class AppModule {}
