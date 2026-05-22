ceb11e15c3f6d4be70c3d09eaa9bc46d949bba21
Tonsak <tonsak.gs@gmail.com>
2026-05-22 21:17:44 +0700

    feat: enhance RolesGuard to check for public access before role validation

--- api/src/features/auth/guards/roles.guard.ts
+++ api/src/features/auth/guards/roles.guard.ts
@@ -2,19 +2,29 @@ import { Reflector } from "@nestjs/core";
 import { UserRole } from "@/features/users/entities/user.entity";
 import { ROLES_KEY } from "@/features/auth/decorators/roles.decorator";
 import { Injectable, CanActivate, ExecutionContext } from "@nestjs/common";
+import { IS_PUBLIC_KEY } from "@/features/auth/decorators/public.decorator";
 
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private readonly reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass()
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
     const roles = this.reflector.getAllAndOverride<UserRole[]>(ROLES_KEY, [
       context.getHandler(),
       context.getClass()
     ]);
 
     if (!roles) {
-      return true;
+      return false;
     }
 
     const { user } = context.switchToHttp().getRequest();
