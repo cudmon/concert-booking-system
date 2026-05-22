2a83bca3d72ba46a3bbfd74a596eac085781317b
Tonsak <tonsak.gs@gmail.com>
2026-05-22 21:17:34 +0700

    feat: add response interceptor to handle error messages in axios instance

--- web/src/libs/http.ts
+++ web/src/libs/http.ts
@@ -4,3 +4,19 @@ export const http = axios.create({
   baseURL: "/api",
   withCredentials: true
 });
+
+http.interceptors.response.use(
+  (response) => response,
+
+  (error) => {
+    if (error.response) {
+      const message = error.response.data.message || error.response.data.error;
+
+      if (message) {
+        error.message = message;
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
