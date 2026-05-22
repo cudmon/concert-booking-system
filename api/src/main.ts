440f37126c9141064893ca0da836825945a3d6d3
Tonsak <tonsak.gs@gmail.com>
2026-05-22 13:02:10 +0700

    feat: add cookie-parser dependency and integrate it in main application

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,12 @@
+import cookie from "cookie-parser";
 import { AppModule } from "@/app.module";
 import { NestFactory, Reflector } from "@nestjs/core";
 import { ClassSerializerInterceptor, ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookie());
 
   app.enableCors();
 
