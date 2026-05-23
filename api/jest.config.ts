e7a09e1f32ccecf2940978cb9a74d191a4d0959d
Tonsak <tonsak.gs@gmail.com>
2026-05-23 11:53:27 +0700

    feat: add moduleNameMapper configuration for path aliasing in Jest

--- api/jest.config.ts
+++ api/jest.config.ts
@@ -3,6 +3,9 @@ export default {
   testEnvironment: "node",
   testRegex: String.raw`.*\.spec\.ts$`,
   moduleFileExtensions: ["js", "json", "ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1"
+  },
   transform: {
     "^.+\\.(t|j)s$": "ts-jest"
   }
